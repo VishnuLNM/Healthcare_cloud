3eaf8d761f22aa7d4e443fdeb6de912b7df31f45
VishnuLNM <vplnm2507@gmail.com>
2026-06-01 11:27:15 +0530

    Set up AWS infrastructure with VPC and subnets
    Added AWS provider, VPC, subnet, internet gateway, and route table resources.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,30 @@
+provider "aws" {
+ region = "us-east-2"
+}
 
+resource "aws_vpc" "main" {
+ cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main" {
+ vpc_id = aws_vpc.main.id
+ cidr_block = "10.0.1.0/24"
+ map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "gw" {
+ vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "rt" {
+ vpc_id = aws_vpc.main.id
+
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.gw.id
+ }
+}
+
+locals {
+ instance_names = ["master","node","monitor"]
+}
